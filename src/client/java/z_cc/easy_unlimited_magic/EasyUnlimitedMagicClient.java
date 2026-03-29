84fdcb0a815f25e4f87eda6ac8b914413a965617
ZecCMachine <zackery35melek@gmail.com>
2026-03-29 11:57:51 -0500

    init project setup

---
+++ src/client/java/z_cc/easy_unlimited_magic/EasyUnlimitedMagicClient.java
@@ -0,0 +1,10 @@
+package z_cc.easy_unlimited_magic;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EasyUnlimitedMagicClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
